c57379dbf768859c87b8f51920e49c3b389ed4f2
Shreesha Raj <shreesha@SHREESH-MAC151629395.local>
2018-11-19 20:32:57 +0530

    apis 1,2 and 4

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-apis
+REST APIs examples
+
+1. GET /read-file returns this file, i.e., reads out the entire README file
+
+2. GET /product accepts two paramenters num1 and num2 as query strings in the URL and prints the product of the two numbers.
+EX: http://localhost:3003/product?num1=2&num2=37
+
+3. GET /non-repeat-char accepts one parameter str and prints the non repeating char in str as output
---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3003
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/read-file', (request, response) => {
+    var fs = require('fs'),
+        path = require('path'),    
+        filePath = path.join(__dirname, 'README.md');
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err) {
+            console.log('received data: ' + data);
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            response.end();
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/product', (request, response) => {
+    console.log('received data: ' + JSON.stringify(request.query));
+    const product = (request.query.num1*request.query.num2).toString();
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(product);
+    response.end();
+});
+
+app.get('/non-repeat-char', (request, response) => {
+    console.log('received data: ' + JSON.stringify(request.query));
+    const str = request.query.str;
+    var arr = str.split('');
+    var result = '';
+    var ctr = 0; 
+    for (var x = 0; x < arr.length; x++) {
+        ctr = 0;
+        for (var y = 0; y < arr.length; y++) {
+            if (arr[x] === arr[y]) {
+                ctr+= 1;
+            }
+        }
+        if (ctr < 2) {
+            result = arr[x];
+            break;
+        }
+    }
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(result);
+    response.end();
+});
